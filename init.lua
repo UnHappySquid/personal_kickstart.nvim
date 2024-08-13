cec7060711561e62f6ff3a6842052bab1cad2dc7
UnHappySquid <imad.issafras@mail.mcgill.ca>
2024-08-13 11:40:32 -0400

    addded quick save with <C-s> from insert mode

--- init.lua
+++ init.lua
@@ -181,6 +181,9 @@ vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
+-- Added quick save option
+vim.keymap.set("i", "<C-s>", "<C-o><cmd>w<CR>")
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
